566d4ccf94c817bb3d0a5f03454a30c23ed2134f
vaibhav7024 <vjaiswal7024@gmail.com>
2026-09-15 12:02:22 +0530

    Add: valid-palindrome-ii - submission-3

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-3.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool ispalindrome(string& s, int i, int j){
+        while(i<j){
+            if(s[i]!=s[j]){
+                return false;
+            }
+            i++;j--;
+        }
+        return true;
+    }
+    bool validPalindrome(string s) {
+        if(ispalindrome(s,0,s.size()-1)) return true;
+        int count=0;
+        int i=0;
+        int j=s.size()-1;
+        while(i<j){
+            if(s[i]!=s[j]){
+                return ispalindrome(s,i+1,j) || ispalindrome(s,i,j-1);
+            }
+            i++;j--;
+        }
+        return true;
+    }
+};
